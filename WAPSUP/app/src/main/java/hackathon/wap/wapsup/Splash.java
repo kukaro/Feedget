dc5a02d647cfba3f7b62a52179be1f8c952af64b
dl57934 <dl57934@naver.com>
2017-09-27 17:25:20 +0900

    login & join success

--- WAPSUP/app/src/main/java/hackathon/wap/wapsup/Splash.java
+++ WAPSUP/app/src/main/java/hackathon/wap/wapsup/Splash.java
@@ -1,8 +1,10 @@
 package hackathon.wap.wapsup;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 
 public class Splash extends AppCompatActivity {
@@ -11,9 +13,8 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_NETWORK_STATE, Manifest.permission.INTERNET, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 3);//권한 주기
         Handler handler = new Handler();
-
-
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
